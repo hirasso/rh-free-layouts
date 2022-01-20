7e006d6e345d9edbd24a01fcebd6e9c6e3130d27
Rasso Hilber <mail@rassohilber.com>
2022-01-20 10:33:34 +0100

    V 1.3.6

--- rh-free-layouts.php
+++ rh-free-layouts.php
@@ -1,7 +1,7 @@
 <?php 
 /**
  * Plugin Name: RH Free Layouts
- * Version: 1.3.5
+ * Version: 1.3.6
  * Author: Rasso Hilber
  * Description: Free drag-and-drop layouts 
  * Author URI: https://rassohilber.com
